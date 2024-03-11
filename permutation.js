2753fe1717bf04cda84658255ab4977307a1349e
Meow <160713239+learner-nayan@users.noreply.github.com>
2024-03-11 11:24:57 +0000

    permutation -number of possible combinations of given string characters

---
+++ permutation.js
@@ -0,0 +1,50 @@
+/**
+ * @param {string} tiles
+ * @return {number}
+ */
+
+let strLen;
+let arr = [];
+const numTilePossibilities = function(tiles) {
+    strLen = tiles.length
+    let usableTiles = tiles
+    for (const char of usableTiles) {
+        let count = 0
+        for (const checkChar of usableTiles) {
+                if(char === checkChar){
+                    count++
+                    usableTiles = usableTiles.replace(checkChar, '')
+                }
+        }
+        if(count>1){
+            arr.push(count)
+        }
+    }
+};
+
+const fact = function(num){
+    if(num === 1){
+        return 1;
+    }else{
+        return num * fact(num-1)
+    }
+}
+
+
+let input = "ABC"
+numTilePossibilities(input)
+console.log(arr)
+
+let factorialOfStringLength = fact(strLen) 
+let factorialOfDenominator = 1;
+
+arr.forEach(element => {
+    factorialOfDenominator = factorialOfDenominator * fact(element)
+});
+
+let finalPermutation = factorialOfStringLength / factorialOfDenominator
+
+console.log(finalPermutation);
+// console.log(factorialOfStringLength)
+// console.log(factorialOfDenominator)
+
